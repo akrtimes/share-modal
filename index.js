08047f9ce3d2be7282735243696bfd9ae663dc26
foolish coder <122173165+foolishcoder97@users.noreply.github.com>
2023-07-07 00:57:57 +0530

    share modal app for sharing social media

---
+++ index.js
@@ -0,0 +1,30 @@
+// let shareprofilecontainer=document.querySelector('#shareprofile-container');
+
+function openSharefun(){
+    document.querySelector(".share-profile").classList.add("class","abc");
+    document.querySelector(".overlay").classList.add("class","xyz");
+}
+
+function removeShareFun(){
+    document.querySelector(".share-profile").classList.remove("abc");
+    document.querySelector(".overlay").classList.remove("class","xyz");
+}
+
+
+
+
+// const modal = document.querySelector(".modal");
+// const overlay = document.querySelector(".overlay");
+
+// //   Modal open function
+// const openModal = () => {
+//   console.log("Modal is Open");
+//   modal.classList.add("active");
+//   overlay.classList.add("overlayactive");
+// };
+
+// // Modal close function
+// const closeModal = () => {
+//   modal.classList.remove("active");
+//   overlay.classList.remove("overlayactive");
+// };
